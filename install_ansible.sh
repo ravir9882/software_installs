8271dccb16126b0bb9f6e6adeefa23f311628995
EC2 Default User <ec2-user@ip-172-31-81-251.ec2.internal>
2020-03-31 09:28:30 +0000

    adding ansible install script

---
+++ install_ansible.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum update -y
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+yum repolist
+yum — enablerepo=epel install ansible
+yum install ansible
